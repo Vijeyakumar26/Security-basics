7a896db00026bbad7cdb35ded57638ada626d663
Vijeyakumar R <vijeyakumar.26@gmail.com>
2021-06-25 20:13:29 +0530

    Added PasswordEncoder & Role Authorization

--- security-demo/src/main/java/com/security/SecurityConfiguration.java
+++ security-demo/src/main/java/com/security/SecurityConfiguration.java
@@ -1,15 +1,31 @@
 package com.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("user1").password("user1").roles("USER").and()
-                                     .withUser("admin").password("admin").roles("USER");
+        auth.inMemoryAuthentication().withUser("poo").password("poo").roles("USER").and()
+                                     .withUser("admin").password("admin").roles("ADMIN");
+    }
+    @Bean
+    public PasswordEncoder getPasswordEncoder()
+    {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/admin").hasRole("ADMIN").
+                                 antMatchers("/user").hasAnyRole("USER","ADMIN").
+                                 antMatchers("/").permitAll().and().formLogin();
     }
 }
